fb6a8b8fdb98ec4f2168075efd618326e7e9c73e
PraeyaSaraswat <pragyasaraswat1306@gmail.com>
2025-06-30 16:21:41 +0530

    Add files via upload
    question on variables.

---
+++ practice.c
@@ -0,0 +1,45 @@
+//#include<stdio.h>
+//int main (){
+    //int a = 12 ;
+    //int b = 12 ;
+    //int sum = a+b ;
+    //printf("sum = %d", sum);
+    //return 0 ;
+//}
+
+
+//question 2
+//#include<stdio.h>
+//int main(){
+    //int length = 8;
+    //int breadth = 5;
+    //int area = length*breadth;
+    //printf(" Area = %d" , area);
+    //return 0 ;
+//}
+
+
+//#include<stdio.h>
+//int main (){
+     //char a = 'A';
+     //printf("%c", a);
+    
+//}
+
+
+
+//#include<stdio.h>
+//int main (){
+    //float a = 99.99;
+    //printf("%f", a);
+    //return 0 ;
+
+//}
+    
+
+#include<stdio.h>
+int main (){
+    int age = 19;
+    char name = 'P';
+    printf("%d, %c" , age , name );
+}
