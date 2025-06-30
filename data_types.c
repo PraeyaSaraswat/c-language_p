941d16bfb56e56c5a3d139b7ceb45a7acfc0d1cc
PraeyaSaraswat <pragyasaraswat1306@gmail.com>
2025-06-30 15:39:53 +0530

    Add files via upload
    learning c language

---
+++ data_types.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+int main()
+{
+    // int a;
+    // a = 7;
+    int a = 7;
+
+    // float b;
+    // b = 3.14;
+    float b = 3.14;
+
+    // char c;
+    // c = 'a';
+    char c = 'a';
+}
