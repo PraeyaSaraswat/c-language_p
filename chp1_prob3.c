cc0f812719a45bc9b8d0d434b6c6a44ea38e6979
PraeyaSaraswat <pragyasaraswat1306@gmail.com>
2025-07-02 01:57:53 +0530

    Add files via upload
    CELSIUS TO FAHRENHEIT

---
+++ chp1_prob3.c
@@ -0,0 +1,8 @@
+#include<stdio.h>
+int main(){
+    int celsius = 40;
+    int fahrenheit = celsius * 1.8 + 32 ;
+    printf("The value of temperature in fahrenheit is: %d" , fahrenheit);
+}
+
+//CELSIUS TO FAHRENHEIT CONVERT
