608371ef6113c74aa51754e60682f0b5dfcab58c
PraeyaSaraswat <pragyasaraswat1306@gmail.com>
2025-07-02 02:07:35 +0530

    Add files via upload

---
+++ chp1_prob4.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main(){
+    int principal = 1000;
+    int rate = 5;
+    int time = 2;
+    int si = principal * rate * time/100;
+
+    printf("The value of simple intrest is:%d", si );
+
+}
