b095c98d00956451aaebf924cd1a70f4e5af122f
PraeyaSaraswat <pragyasaraswat1306@gmail.com>
2025-07-02 03:17:11 +0530

    Add files via upload

---
+++ arithmetic_instructions.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+int main (){
+int a = 4;
+int b = 3;
+int c = a + b ;
+printf("The value of a is %d and the value of b is %d and the sum is %d\n" , a,b,c);
+printf("The remainder when a is divided by b is : %d\n" , a%b);
+return 0 ;
+}
---
+++ type_conversion.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main(){
+    float a = 9.0;
+    int b = 2.0;
+    float c = 9.0/2.0;
+    printf("The value of a/b is %f", c);
+    return 0;
+}
+//int and int = int
+//float and int = float
+//float and float = float
+// int a = 3.5;  it will demoted to 3
+// float b = 8;  it will promoted to 8.0
